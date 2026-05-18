0ae0b3a4a60d8aac20e92b3dd1244ebfddfbc10e
onyx1212 <a16hamdan@gmail.com>
2026-05-18 20:37:26 +0300

    fix: remove DB import from client, fix intents

--- src/client.js
+++ src/client.js
@@ -1,5 +1,4 @@
 const { Client, GatewayIntentBits, Partials, Options } = require('discord.js');
-const config = require('./config');
 const CommandHandler = require('./handlers/CommandHandler');
 const EventHandler = require('./handlers/EventHandler');
 const { createContextLogger } = require('./utils/Logger');
@@ -10,24 +9,16 @@ function createClient() {
   const client = new Client({
     intents: [
       GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMembers,
       GatewayIntentBits.GuildEmojisAndStickers,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.GuildPresences,
     ],
-    partials: [Partials.Guild, Partials.Channel, Partials.Message],
+    partials: [Partials.Guild, Partials.Channel],
     makeCache: Options.cacheWithLimits({
       MessageManager: 50,
       PresenceManager: 0,
-      GuildMemberManager: {
-        maxSize: 200,
-        keepOverLimit: member => member.id === member.client.user?.id,
-      },
     }),
     sweepers: {
       ...Options.DefaultSweeperSettings,
       messages: { interval: 60, lifetime: 1800 },
-      users: { interval: 3600, filter: () => user => !user.bot },
     },
     rest: {
       timeout: 15000,
@@ -47,19 +38,15 @@ function createClient() {
     setTimeout(() => process.exit(1), 1000);
   });
 
-  process.on('SIGTERM', async () => {
+  process.on('SIGTERM', () => {
     log.info('SIGTERM received, shutting down gracefully...');
     client.destroy();
-    const { disconnect } = require('./database/connection');
-    await disconnect();
     process.exit(0);
   });
 
-  process.on('SIGINT', async () => {
+  process.on('SIGINT', () => {
     log.info('SIGINT received, shutting down gracefully...');
     client.destroy();
-    const { disconnect } = require('./database/connection');
-    await disconnect();
     process.exit(0);
   });
 
